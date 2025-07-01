413bd486280ce88acc27bf00f875b5eb1acb626b
yangxj96 <yangxj96@gmail.com>
2025-07-01 17:00:46 +0800

    prea: update

---
+++ build.rs
@@ -0,0 +1,13 @@
+#[cfg(windows)]
+extern crate winres;
+
+#[cfg(windows)]
+fn main(){
+    if cfg!(target_os = "windows") {
+        let res = winres::WindowsResource::new();
+        // res.set_icon("assets/icon.ico");
+        res.compile().unwrap();
+    } else {
+        println!("当前操作系统不是 Windows，跳过资源编译。");
+    }
+}
