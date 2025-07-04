d014a5cbcf1b586222835bdc2bf0a2d161228f29
yangxj96 <yangxj96@gmail.com>
2025-07-04 17:12:35 +0800

    feat: 构架修改

--- core/build.rs
+++ core/build.rs
@@ -5,7 +5,6 @@ extern crate winres;
 fn main(){
     if cfg!(target_os = "windows") {
         let res = winres::WindowsResource::new();
-        // res.set_icon("assets/icon.ico");
         res.compile().unwrap();
     } else {
         println!("当前操作系统不是 Windows，跳过资源编译。");
